f571921eb8ebc7b29b6eb6705f421c3784e44524
FrancoViniciusP <vpacheco0427@gmail.com>
2022-12-14 11:36:01 -0300

    add dto classes

--- Models/Post.cs
+++ Models/Post.cs
@@ -11,3 +11,22 @@ public class Post
     public int StudentId { get; set; }
     public Student Student { get; set; } = null!;
 }
+
+public class PostDTO
+{
+    public string Text { get; set; } = null!;
+}
+
+public class PostDTOStudent
+{
+    public int PostId { get; set; }
+    public string Text { get; set; } = null!;
+    public string StudentName { get; set; } = null!;
+}
+
+public class PostInsert
+{
+    public int PostId { get; set; }
+    public string Text { get; set; } = null!;
+    public int StudentId { get; set; }
+}
--- Models/Student.cs
+++ Models/Student.cs
@@ -10,3 +10,22 @@ public class Student
     public string Password { get; set; }
     public ICollection<Post> Posts { get; set; }
 }
+
+public class StudentDTO
+{
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Level { get; set; }
+    public string Password { get; set; }
+    public ICollection<Post> Posts { get; set; }
+}
+
+public class StudentDTONew
+{
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Level { get; set; }
+    public string Password { get; set; }
+}
